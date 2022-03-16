34d581cbe928d7c4699ebcb914decc84547c3e59
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2022-03-16 23:29:14 +0330

    transactionsInfo stat added to ExpenseTrackerApp component

--- src/components/expenseTrackerApp/expenseTrackerApp.jsx
+++ src/components/expenseTrackerApp/expenseTrackerApp.jsx
@@ -1,13 +1,22 @@
 import styles from './expenseTrackerApp.module.scss';
 import TransactionList from '../transactionList/transactionList';
 import TransactionOverView from '../transactionOverView/transactionOverView';
-
+import { useState } from 'react';
 
 const ExpenseTrackerApp = () => {
-  return <div className={styles.expenseTrackerAppContainer}>
-      <TransactionOverView/>
-      <TransactionList/>
-  </div>;
+  const [transactionsInfo, setTransactionsInfo] = useState({
+    income: 0,
+    outcome: 0,
+    balance: 0,
+    transactions: [],
+  });
+  
+  return (
+    <div className={styles.expenseTrackerAppContainer}>
+      <TransactionOverView info={transactionsInfo} />
+      <TransactionList />
+    </div>
+  );
 };
 
 export default ExpenseTrackerApp;
