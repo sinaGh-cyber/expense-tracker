e58f47d63d4b5b6f7271f9b6eb09cd3881ede739
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2022-03-17 23:40:57 +0330

    TransactionOverView component dase added + some styling

--- src/components/transactionOverView/transactionOverView.jsx
+++ src/components/transactionOverView/transactionOverView.jsx
@@ -1,7 +1,36 @@
+import { useState } from 'react';
 import styles from './transactionOverView.module.scss';
 
-const TransactionOverView = () => {
-    return ( <></> );
-}
- 
+const TransactionOverView = ({ info }) => {
+  const [isFormShowed, setIsFormShowed] = useState(false);
+  const onBtnClick = (e) => {
+    setIsFormShowed((prev) => !prev);
+  };
+  return (
+    <div className={styles.overviewContainer}>
+      <div className={styles.balance}>
+        <p>balance: {info.balance} $</p>
+      </div>
+      <div className={styles.formAndBtn}>
+        <button onClick={onBtnClick} className={styles.btn}>
+          {isFormShowed ? 'Cancel' : 'Add'}
+        </button>
+
+        {isFormShowed && <div className="form">form</div> }
+      </div>
+
+      <div className={styles.info}>
+        <div className="income">
+          <p>income:</p>
+          <p>{info.income} $</p>
+        </div>
+        <div className="outcome">
+          <p>expense: </p>
+          <p>{info.outcome} $</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default TransactionOverView;
