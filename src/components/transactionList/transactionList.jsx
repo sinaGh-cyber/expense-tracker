f239d38da893921a6aa83fb1022f0755d69c44c6
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2022-03-21 16:41:25 +0330

    TransactionList jsx coded

--- src/components/transactionList/transactionList.jsx
+++ src/components/transactionList/transactionList.jsx
@@ -1,7 +1,48 @@
 import styles from './transactionList.module.scss';
+import TransactionItem from '../transactionItem/transactionItem';
+import { useState } from 'react';
 
-const TransactionList = () => {
-  return <div className={styles.listContainer}></div>;
+const TransactionList = ({
+  transactions,
+  onSelect,
+  onDelete,
+  selectAllToggler,
+}) => {
+  const [isAllSelected, setIsAllSelected] = useState(false);
+  return (
+    <div className={styles.listContainer}>
+      {!!transactions.length && (
+        <>
+          <div className={styles.buttonGroup}>
+            <button className={styles.delete} onClick={onDelete}>
+              Delete
+            </button>
+            <button
+              onClick={() => {
+                setIsAllSelected((p) => !p);
+                selectAllToggler(isAllSelected);
+              }}
+              className={styles.select}
+            >
+              {isAllSelected ? 'Unselect all' : 'Select all'}
+            </button>
+          </div>
+          <ul>
+            {transactions.map((transactionInfo) => {
+              return (
+                <TransactionItem
+                  key={transactionInfo.id}
+                  transaction={transactionInfo}
+                  onSelect={onSelect}
+                />
+              );
+            })}
+          </ul>
+        </>
+      )}
+      {!transactions.length && <p>add a transaction</p>}
+    </div>
+  );
 };
 
 export default TransactionList;
