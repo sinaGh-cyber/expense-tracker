9c0c45b15ca086b70e75af1252553c193c186bb1
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2022-03-21 16:39:30 +0330

    ExpenseTrackerApp onSelect, onDelete and selectAllToggler funcs added + passed as props to TransactionList

--- src/components/expenseTrackerApp/expenseTrackerApp.jsx
+++ src/components/expenseTrackerApp/expenseTrackerApp.jsx
@@ -11,6 +11,45 @@ const ExpenseTrackerApp = () => {
     transactions: [],
   });
 
+  const selectAllToggler = (isAllSelected) => {
+    const transactionsInfoClone = { ...transactionsInfo };
+    transactionsInfoClone.transactions.forEach((transaction) => {
+      transaction.isSelected = !isAllSelected;
+    });
+    setTransactionsInfo(transactionsInfoClone);
+  };
+
+  const onSelect = (id) => {
+    const transactionIdx = transactionsInfo.transactions.findIndex(
+      (transaction) => {
+        return transaction.id === id;
+      }
+    );
+
+    const transactionsInfoClone = { ...transactionsInfo };
+    transactionsInfoClone.transactions[transactionIdx].isSelected =
+      !transactionsInfoClone.transactions[transactionIdx].isSelected;
+    setTransactionsInfo(transactionsInfoClone);
+  };
+
+  const onDelete = () => {
+    const transactionsInfoClone = { ...transactionsInfo };
+    transactionsInfoClone.transactions.forEach((transaction) => {
+      if (transaction.isSelected) {
+        transactionsInfoClone[transaction.type] -= +transaction.amount;
+      }
+    });
+    transactionsInfoClone.balance =
+      transactionsInfoClone.income - transactionsInfoClone.outcome;
+
+    transactionsInfoClone.transactions =
+      transactionsInfoClone.transactions.filter(
+        (transaction) => !transaction.isSelected
+      );
+
+    setTransactionsInfo(transactionsInfoClone);
+  };
+
   const submitHandler = (formInfo) => {
     const transactionsInfoClone = { ...transactionsInfo };
     transactionsInfoClone.transactions.push(formInfo);
@@ -27,7 +66,12 @@ const ExpenseTrackerApp = () => {
         info={transactionsInfo}
         submitHandler={submitHandler}
       />
-      <TransactionList transactions={transactionsInfo.transactions} />
+      <TransactionList
+        onSelect={onSelect}
+        onDelete={onDelete}
+        selectAllToggler={selectAllToggler}
+        transactions={transactionsInfo.transactions}
+      />
     </div>
   );
 };
